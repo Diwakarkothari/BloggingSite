5d613e7604b3297422517eb241eb2f435e7987d5
Diwakarkothari <diwakarkothari810@gmail.com>
2024-09-02 14:25:01 +0530

    addBlog

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const path = require('path');
 const cookieparser = require('cookie-parser');
 
+const Blog=require('./models/blog');
+
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/blogify').
     then(console.log("MongoDB Connected Successfully"));
 
 const userRoutes = require('./routes/user');
+const blogRoutes = require('./routes/blog');
+
 const checkForAuthentication = require('./middlewares/authentication');
 
 const app=express();
@@ -16,15 +20,19 @@ const PORT = 8000;
 app.use(express.urlencoded({extended:false}));
 app.use(cookieparser())
 app.use(checkForAuthentication("token"));
+app.use(express.static(path.resolve('./public')));
 
 app.set('view engine','ejs');
 app.set('views', path.resolve('./views'));
 
 app.use("/user",userRoutes);
+app.use('/blog',blogRoutes);
 
-app.get('/',(req,res)=>{
+app.get('/', async(req,res)=>{
+    const allblogs = await Blog.find({});
     res.render('home',{
         user:req.user,
+        blogs:allblogs
     });
 })
 
--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -3,14 +3,14 @@ function checkForAuthentication(cookiename)
 {
     return (req,res,next)=>{
         const value = req.cookies[cookiename];
-        console.log("token "+value);
+        // console.log("token "+value);
         if(!value)
         {
             return next(); 
         }
 
         const payload = checkToken(value);
-        console.log('payload: ',payload);
+        // console.log('payload: ',payload);
         req.user=payload;
         res.locals.user = req.user;
             // console.log('Cookies:', req.cookies);
