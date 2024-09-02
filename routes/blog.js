f5a099a328a0545fa3f27a8693ba7e640ccbbc28
Diwakarkothari <diwakarkothari810@gmail.com>
2024-09-02 15:12:10 +0530

    blog

--- routes/blog.js
+++ routes/blog.js
@@ -2,6 +2,7 @@ const {Router} = require('express');
 const multer = require('multer');
 const path = require('path');
 const Blog= require('../models/blog');
+const Comment = require('../models/comments');
 
 const router = Router();
 
@@ -23,6 +24,14 @@ router.get('/add-new',(req,res)=>{
     });
 })
 
+router.get('/:id', async (req,res)=>{
+    const blog = await Blog.findById(req.params.id).populate('createdBy');
+    return res.render('blog',{
+        user:req.user,
+        blog:blog
+    })
+})
+
 router.post('/',upload.single('coverImage'),async(req,res)=>{
     // console.log(req.body);
     const {title,body} = req.body
@@ -33,6 +42,15 @@ router.post('/',upload.single('coverImage'),async(req,res)=>{
         createdBy:req.user._id
     })
     return res.redirect(`/blog/${blog._id}`);
+})
+
+router.post('/comment/:blogId', async(req,res)=>{
+    const comment = await Comment.create({
+        content:req.body.content,
+        blogId:req.params.blogId,
+        createdBy:req.user._id
+    })
+    return res.redirect(`/blog/${req.params.blogId}`);
 })
 
 module.exports=router;
